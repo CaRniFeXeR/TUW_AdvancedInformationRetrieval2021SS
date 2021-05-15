336432bfbb0d66cba3ff2609b648503343e002df
ChrisPrein <e01529038@student.tuwien.ac.at>
2021-05-15 21:17:34 +0200

    adds default implementation for conv-knrm

--- src/model_conv_knrm.py
+++ src/model_conv_knrm.py
@@ -6,6 +6,7 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 
 from allennlp.modules.text_field_embedders import TextFieldEmbedder
+from allennlp.modules.matrix_attention.cosine_matrix_attention import CosineMatrixAttention              
 
 class Conv_KNRM(nn.Module):
     '''
@@ -27,6 +28,25 @@ class Conv_KNRM(nn.Module):
         self.sigma = Variable(torch.FloatTensor(self.kernel_sigmas(n_kernels)), requires_grad=False).view(1, 1, 1, n_kernels)
 
         # todo
+        self.convolutions = []
+        for i in range(1, n_grams + 1):
+            self.convolutions.append(
+                nn.Sequential(
+                    nn.ConstantPad1d((0,i - 1), 0),
+                    nn.Conv1d(kernel_size=i, in_channels=word_embeddings.get_output_dim(), out_channels=conv_out_dim),
+                    nn.ReLU()) 
+            )
+        self.convolutions = nn.ModuleList(self.convolutions) # register conv as part of the model
+
+
+        # this does not really do "attention" - just a plain cosine matrix calculation (without learnable weights) 
+        self.cosine_module = CosineMatrixAttention()
+
+        # *9 because we concat the 3x3 conv match sums together before the dense layer
+        self.dense = nn.Linear(n_kernels * n_grams * n_grams, 1, bias=False) 
+
+        # init with small weights, otherwise the dense output is way to high fot
+        torch.nn.init.uniform_(self.dense.weight, -0.014, 0.014)  # inits taken from matchzoo
 
     def forward(self, query: Dict[str, torch.Tensor], document: Dict[str, torch.Tensor]) -> torch.Tensor:
 
@@ -46,8 +66,73 @@ class Conv_KNRM(nn.Module):
         document_embeddings = self.word_embeddings(document)
 
         #todo
+        
+        # !! conv1d requires tensor in shape: [batch, emb_dim, sequence_length ]
+        # so we transpose embedding tensors from : [batch, sequence_length,emb_dim] to [batch, emb_dim, sequence_length ]
+        # feed that into the conv1d and reshape output from [batch, conv1d_out_channels, sequence_length ] 
+        # to [batch, sequence_length, conv1d_out_channels]
 
+        query_embeddings_t = query_embeddings.transpose(1, 2)
+        document_embeddings_t = document_embeddings.transpose(1, 2)
+
+        query_results = []
+        document_results = []
+
+        for i,conv in enumerate(self.convolutions):
+            query_conv = conv(query_embeddings_t).transpose(1, 2) 
+            document_conv = conv(document_embeddings_t).transpose(1, 2)
+
+            query_results.append(query_conv)
+            document_results.append(document_conv)
+
+        matched_results = []
+
+        for i in range(len(query_results)):
+            for t in range(len(query_results)):
+                matched_results.append(self.forward_matrix_kernel_pooling(query_results[i], document_results[t], query_by_doc_mask, query_pad_mask))
+
+        #
+        # "Learning to rank" layer
+        # -------------------------------------------------------
+
+        all_grams = torch.cat(matched_results,1)
+
+        dense_out = self.dense(all_grams)
+        #tanh_out = torch.tanh(dense_out)
+
+        output = torch.squeeze(dense_out, 1)
         return output
+
+    #
+    # create a match matrix between query & document terms
+    #
+    def forward_matrix_kernel_pooling(self, query_tensor, document_tensor, query_by_doc_mask, query_pad_oov_mask):
+
+        #
+        # cosine matrix
+        # -------------------------------------------------------
+        # shape: (batch, query_max, doc_max)
+        
+        cosine_matrix = self.cosine_module.forward(query_tensor, document_tensor)
+        cosine_matrix_masked = cosine_matrix * query_by_doc_mask
+        cosine_matrix_extradim = cosine_matrix_masked.unsqueeze(-1)
+
+        #
+        # gaussian kernels & soft-TF
+        #
+        # first run through kernel, then sum on doc dim then sum on query dim
+        # -------------------------------------------------------
+        
+        raw_kernel_results = torch.exp(- torch.pow(cosine_matrix_extradim - self.mu, 2) / (2 * torch.pow(self.sigma, 2)))
+        kernel_results_masked = raw_kernel_results * query_by_doc_mask.unsqueeze(-1)
+
+        per_kernel_query = torch.sum(kernel_results_masked, 2)
+        log_per_kernel_query = torch.log(torch.clamp(per_kernel_query, min=1e-10)) * 0.01
+        log_per_kernel_query_masked = log_per_kernel_query * query_pad_oov_mask.unsqueeze(-1) # make sure we mask out padding values
+
+        per_kernel = torch.sum(log_per_kernel_query_masked, 1) 
+
+        return per_kernel
 
     def kernel_mus(self, n_kernels: int):
         """
