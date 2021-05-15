90a7525c726700e5ba331a8685beb5a9d55fda86
Florian Kowarsch <florian.kowarsch@tuwien.ac.at>
2021-05-15 21:29:06 +0200

    added masking to tk, added shape comments to the code

--- src/model_tk.py
+++ src/model_tk.py
@@ -37,8 +37,10 @@ class TK(nn.Module):
         self.cosinematrix = CosineMatrixAttention()
 
         # Contextualization
+        # n_layers of transformers stored in a List
         self.ffLayers: List[FeedForward] = []
         self.attentionHeadLayers: List[MultiHeadSelfAttention] = []
+        self.normLayers : List[LayerNorm] = []
         for i in range(n_layers):
             self.ffLayers.append(
                 FeedForward(
@@ -56,11 +58,12 @@ class TK(nn.Module):
                     attention_dim=30,
                     values_dim=30
                 ))
-        self.layer_norm = LayerNorm(n_tf_dim)
+            self.normLayers.append(LayerNorm(n_tf_dim))
 
         self.linear_Slog = nn.Linear(n_kernels, 1, bias=False)
         self.linear_Slen = nn.Linear(n_kernels, 1, bias=False)
 
+        # should be value between 0 and 1
         self.alpha = nn.parameter.Parameter(torch.tensor(0.5))  # alpha --> to control amount contextualization
         self.beta = nn.parameter.Parameter(torch.tensor(0.5))  # beta --> to control amount of s_log on the score
         self.gamma = nn.parameter.Parameter(torch.tensor(0.5))  # gamma --> to control amount of s_len on the score
@@ -78,6 +81,7 @@ class TK(nn.Module):
         query_pad_oov_mask_bool = query["tokens"] > 0
         query_pad_oov_mask = query_pad_oov_mask_bool.float()  # > 1 to also mask oov terms
         # shape: (batch, doc_max)
+        # doc_max --> maximal document length of the longest document in the batch --> each other document is padded with zeros
         document_pad_oov_mask_bool = document["tokens"] > 0
         document_pad_oov_mask = document_pad_oov_mask_bool.float()
 
@@ -93,9 +97,10 @@ class TK(nn.Module):
         # query & document is processed separately --> learn parameters are shared
 
         # 1. positional embedding added --> p
+        #query_embeddings_pos: (batch_size, query_len, embedding_dim)
         query_embeddings_pos = add_positional_features(query_embeddings)
+        #document_embeddings_pos: (batch_size, document_len, embedding_dim)
         document_embeddings_pos = add_positional_features(document_embeddings)
-        # todo include pad?
 
         # 2 transformer layers
 
@@ -104,25 +109,41 @@ class TK(nn.Module):
         document_contextualized = document_embeddings_pos
 
         # n transformer blocks
-        for ff, mutlihead in zip(self.ffLayers, self.attentionHeadLayers):
+        for ff, mutlihead, layer_norm in zip(self.ffLayers, self.attentionHeadLayers, self.normLayers):
             # feedforward layer
             ff_query_contextualized = ff(query_contextualized)
             ff_document_contextualized = ff(document_contextualized)
             # mutlihead attention (provide padding mask)
             attenion_query_contextualized = mutlihead(ff_query_contextualized, query_pad_oov_mask_bool)
             attenion_document_contextualized = mutlihead(ff_document_contextualized, document_pad_oov_mask_bool)
-            query_contextualized = self.layer_norm(attenion_query_contextualized + ff_query_contextualized)
-            document_contextualized = self.layer_norm(attenion_document_contextualized + ff_document_contextualized)
+            query_contextualized = layer_norm(attenion_query_contextualized + ff_query_contextualized)
+            document_contextualized = layer_norm(attenion_document_contextualized + ff_document_contextualized)
 
         # ^t_i =t_i * alpha + context(t1:n)_i * (1- alpha) --> alpha controls the influence of contextualization --> is also learned
-        query_embedded_contextualized = (self.alpha * query_embeddings) + ( 1- self.alpha) * query_contextualized
+        query_embedded_contextualized =(self.alpha * query_embeddings) + ( 1- self.alpha) * query_contextualized
         document_embedded_contextualized = (self.alpha * document_embeddings_pos) + ( 1- self.alpha) * document_contextualized
+
+        # the contextualizations adds values to the words that are only padded (therefore we need to remove the values again by multipyling with the paddding masks)
+        query_embedded_contextualized = query_embedded_contextualized * query_pad_oov_mask.unsqueeze(-1)
+        document_embedded_contextualized = document_embedded_contextualized * document_pad_oov_mask.unsqueeze(-1)
+        #since we kept the shape intact through out the transformer part have the following shape for the contextulized embeddings
+        #query_embedded_contextualized: (batch_size, query_len, embedding_dim)
+        #document_embedded_contextualized: (batch_size, document_len, embedding_dim)
 
         # intercation scoring
 
         # 1. query sequence and document sequence match in a single match-matrix
         # M_ij = cosine_similarity(q_i,d_j)
+        # query: (batch_size, query_len, embedding_dim) = (batch_size, 14, 300) #for an example input
+        # document: (batch_size, doc_len, embedding_dim) = (batch_size, 180, 300)
+        # cosine matrix m: (batch_size, query_len, doc_len, 1) = (batch_size, 14, 180, 1)
         cosine_matrix_m = self.cosinematrix.forward(query_embedded_contextualized, document_embedded_contextualized)
+
+        #since we now work on values per query term and document term we also need a mask that indicate this padding values in this interaction matrix
+        #query_by_doc_mask: (batch_size, query_len, document_len)
+        query_by_doc_mask = torch.bmm(query_pad_oov_mask.unsqueeze(-1), document_pad_oov_mask.unsqueeze(-1).transpose(-1, -2))
+
+        cosine_matrix_m = cosine_matrix_m * query_by_doc_mask
 
         # todo explain why unsqueez is needed
         cosine_matrix_m = cosine_matrix_m.unsqueeze(-1)
@@ -130,25 +151,39 @@ class TK(nn.Module):
 
         # K^k_ij = exp(-(M_ij _ mu_k)^2 / (2\sigma^2))              ...mu & sigma are from the kenel
         # 3. each kernel results in a kernel matrix K^k
+        # kernel_matrises: (batch_size, query_len, doc_len, n_kernels) = (batch_size, 14, 180, 11)
         kernel_matrises = torch.exp(- torch.pow(cosine_matrix_m - self.mu, 2) / (2 * torch.pow(self.sigma, 2)))
+
+        #masking needed after the kernel 
+        query_by_doc_mask_view = query_by_doc_mask.unsqueeze(-1)
+        kernel_matrises = kernel_matrises * query_by_doc_mask_view
+        
         # 4. document dimension j is summed for each query term and kernel
+        # result_summed_document_axis: (batch_size, query_len, n_kernels) = (batch_size, 14, 11)
+        # because doc is in the 3rd dimension we use torch.sum(m, 2)
         result_summed_document_axis = torch.sum(kernel_matrises, 2)
 
         # The kernel models need masking after the kernels -> the padding 0's will become non-zero, because of the kernel (but when summed up again will distort the output)
 
         # 5a. log normalization
         # log_b is applied on each query term before summing them up resulting in s^k_log
-        log_result_summed_document_axis = torch.log2(torch.clamp(result_summed_document_axis, min=1e-10)) #log2(0) = -inf we therefore need to clamp zero values to a very low values in order to a result != -inf
+        #log2(0) = -inf we therefore need to clamp zero values to a very low values in order to a result != -inf
+        log_result_summed_document_axis = torch.log2(torch.clamp(result_summed_document_axis, min=1e-10)) 
+        #log_result_k: (batch_size, n_kernel) = (batch_size, 11)
+        # since we sumed over all query terms we retrive now one value per kernel
         log_result_k = torch.sum(log_result_summed_document_axis, 1)
         # 5b. length normalization
         # /document_length is applied on each query term before summing them up resulting in s^k_len
-        document_length = document_embeddings.size(0)
+        document_length = document_embeddings.shape[1] #doc_leng 180 in our case
         normed_result_summed_document_axis = result_summed_document_axis / document_length
+         #normed_result_k: (batch_size, n_kernel) = (batch_size, 11)
+        # since we sumed over all query terms we retrive now one value per kernel
         normed_result_k = torch.sum(normed_result_summed_document_axis, 1)
 
         # 6 kernel scores (one value per kernel) is weighted and summed up with simple linear layer (w_log, W_len)
         # results in one scalar for log-normalized and length normalized kernels --> s_log & s_len
 
+        # s_log and s_len --> one value per batch --> shape: (batch_size, 1)
         s_log = self.linear_Slog(log_result_k)
         s_len = self.linear_Slen(normed_result_k)
 
@@ -195,8 +230,9 @@ class TK(nn.Module):
         self.mu = self.mu.cuda()
         self.sigma = self.sigma.cuda()
 
-        for ff, mutlihead in zip(self.ffLayers, self.attentionHeadLayers):
+        for ff, mutlihead, layernrom in zip(self.ffLayers, self.attentionHeadLayers, self.normLayers):
             ff.cuda()
             mutlihead.cuda()
+            layernrom.cuda()
 
         return self.cuda()
