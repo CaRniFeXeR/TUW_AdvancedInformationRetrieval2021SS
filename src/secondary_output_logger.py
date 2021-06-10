190d6414d175f38fe3f760dc90cc2ba3058378e4
ChrisPrein <79170851+ChrisPrein@users.noreply.github.com>
2021-06-10 23:23:21 +0200

    adds model data to secondary data logger

--- src/secondary_output_logger.py
+++ src/secondary_output_logger.py
@@ -8,6 +8,27 @@ from datetime import datetime
 
 ROOT_PATH: Path = Path('logs/')
 
+class ModelData(ABC):
+    @abstractmethod
+    def to_dict(self) -> Dict[str, numpy.ndarray]:
+        pass
+
+class TKModelData(ModelData):
+    def __init__(self, dense_weight, dense_mean_weight, dense_comb_weight):
+        self.dense_weight = dense_weight
+        self.dense_mean_weight = dense_mean_weight
+        self.dense_comb_weight = dense_comb_weight
+
+    def to_dict(self) -> Dict[str, numpy.ndarray]:
+        return {"dense_weight": self.dense_weight, "dense_mean_weight": self.dense_mean_weight, "dense_comb_weight": self.dense_comb_weight}
+
+class FKModelData(ModelData):
+    def __init__(self):
+        pass
+
+    def to_dict(self) -> Dict[str, numpy.ndarray]:
+        return {}
+
 class SecondaryBatchOutput():
     def __init__(self, score: torch.Tensor, per_kernel: torch.Tensor, query_embeddings: torch.Tensor, query_embeddings_oov_mask: torch.Tensor, cosine_matrix: torch.Tensor, query_id: List[str], doc_id: List[str]):
         self.__score: torch.Tensor = score
@@ -41,9 +62,37 @@ class SecondaryBatchOutput():
     def doc_id(self) -> List[str]:
         return self.__doc_id
 
+    def to_dict(self) -> Dict[str, numpy.ndarray]:
+        __secondary_output: Dict[str, numpy.ndarray] = {}
+
+        for sample_index, query_id in enumerate(self.query_id):
+            doc_id: str = self.doc_id[sample_index]
+
+            if not query_id in __secondary_output.keys():
+                __secondary_output[query_id] = {}
+
+            if not doc_id in __secondary_output[query_id].keys():
+                __secondary_output[query_id][doc_id] = {}
+                __secondary_output[query_id][doc_id]["score"] = self.score.cpu()[sample_index].data.numpy()
+                __secondary_output[query_id][doc_id]["per_kernel"] = self.per_kernel.cpu()[sample_index].data.numpy()
+                __secondary_output[query_id][doc_id]["query_mean_vector"] = self.query_mean_vector.cpu()[sample_index].data.numpy()
+                __secondary_output[query_id][doc_id]["cosine_matrix_masked"] = self.cosine_matrix.cpu()[sample_index].data.numpy()
+
+        return __secondary_output
+
 class SecondaryBatchOutputLogger(ABC):
     @abstractmethod
     def log(self, secondary_batch_output: SecondaryBatchOutput):
+        pass
+
+    @property
+    @abstractmethod
+    def model_data(self) -> ModelData:
+        pass
+
+    @model_data.setter 
+    @abstractmethod
+    def model_data(self, value: ModelData):
         pass
 
 class SecondaryBatchOutputFileLoggerConfig():
@@ -62,7 +111,8 @@ class SecondaryBatchOutputFileLogger(SecondaryBatchOutputLogger):
         self.__config: SecondaryBatchOutputFileLoggerConfig = config
 
         self.__create_folder_structure()
-        self.__secondary_output: Dict[str, Any] = {}
+        self.__secondary_output: Dict[str, numpy.ndarray] = {}
+        self.__model_data: ModelData = None
         self.__log_file: IO[Any] = open(self.__file_path, "wb")
 
     def __enter__(self):
@@ -89,19 +139,21 @@ class SecondaryBatchOutputFileLogger(SecondaryBatchOutputLogger):
             
     def __create_folder_structure(self):
         self.__store_path.absolute().mkdir(parents=True, exist_ok=True)
+
+    def __write_data_to_file(self):
+        numpy.savez_compressed(self.__log_file, model_data=(self.__model_data.to_dict() if not self.model_data is None else None), qd_data=self.__secondary_output)
+
+    @property
+    def model_data(self) -> ModelData:
+        return self.__model_data
+
+    @model_data.setter 
+    def model_data(self, value: ModelData):
+        self.__model_data = value
+
+        self.__write_data_to_file()
         
     def log(self, secondary_batch_output: SecondaryBatchOutput):
-        for sample_index, query_id in enumerate(secondary_batch_output.query_id):
-            doc_id: str = secondary_batch_output.doc_id[sample_index]
-
-            if not query_id in self.__secondary_output.keys():
-                self.__secondary_output[query_id] = {}
-
-            if not doc_id in self.__secondary_output[query_id].keys():
-                self.__secondary_output[query_id][doc_id] = {}
-                self.__secondary_output[query_id][doc_id]["score"] = secondary_batch_output.score.cpu()[sample_index].data.numpy()
-                self.__secondary_output[query_id][doc_id]["per_kernel"] = secondary_batch_output.per_kernel.cpu()[sample_index].data.numpy()
-                self.__secondary_output[query_id][doc_id]["query_mean_vector"] = secondary_batch_output.query_mean_vector.cpu()[sample_index].data.numpy()
-                self.__secondary_output[query_id][doc_id]["cosine_matrix_masked"] = secondary_batch_output.cosine_matrix.cpu()[sample_index].data.numpy()
+        self.__secondary_output = {**self.__secondary_output, **secondary_batch_output.to_dict()}
         
-        numpy.savez_compressed(self.__log_file, qd_data=self.__secondary_output)
+        self.__write_data_to_file()
--- src/secondary_output_logging.py
+++ src/secondary_output_logging.py
@@ -10,7 +10,7 @@ from allennlp.data.vocabulary import Vocabulary
 from allennlp.data.dataloader import PyTorchDataLoader
 from re_ranking import evaluateModel
 from core_metrics import calculate_metrics_plain, load_qrels, unrolled_to_ranked_result
-from secondary_output_logger import SecondaryBatchOutput, SecondaryBatchOutputFileLogger, SecondaryBatchOutputFileLoggerConfig, SecondaryBatchOutputLogger
+from secondary_output_logger import SecondaryBatchOutput, SecondaryBatchOutputFileLogger, SecondaryBatchOutputFileLoggerConfig, SecondaryBatchOutputLogger, TKModelData, FKModelData
 from pathlib import Path
 
 def main():
@@ -44,9 +44,11 @@ def main():
         if config["model"] == "tk":
             # "learning_rate": 0.0001 needed for tk to perform
             model = TK(word_embedder, n_kernels=11, n_layers=2, n_tf_dim=300, n_tf_heads=10, tf_projection_dim=30, secondary_batch_output_logger=secondary_output_logger)
+            secondary_output_logger.model_data = TKModelData(dense_weight=None, dense_mean_weight=None, dense_comb_weight=None)
         elif config["model"] == "fk":
             # learning_rate" : 0.001 needed for fk to perform
             model = FK(word_embedder, n_kernels=11, n_layers=2, n_fnet_dim=300, secondary_batch_output_logger=secondary_output_logger)
+            secondary_output_logger.model_data = FKModelData()
         else:
             raise ValueError("no known model configured!")
 
